36d454b9304ca63e4b6c402f5da75e0ceca10a72
2305ray <rayssagarcia035@gmail.com>
2025-03-11 15:22:55 -0300

    começo da estilização do formulário

--- src/components/Pages/Shopping/index.jsx
+++ src/components/Pages/Shopping/index.jsx
@@ -1,82 +1,98 @@
 import React from "react";
+import { MapPinLine, CurrencyDollar } from 'phosphor-react'
+import { ContainerCompletePedido, Container, ContainerEndereçoEntrega, ContentEntrega,
+ ContentPagamento, ContainerForm, Input } from './styles'
 
 export function Shopping() {
     return (
-        <>
-        <section>
-            <h1>Complete seu pedido</h1>
-            <div>
-                <section>
-                    <h3>
-                        Endereço de Entrega
-                    </h3>
-                    <p>Informe o endereço onde deseja receber seu pedido</p>
-                    <form action="">
-                        <input type="text" id="cep" placeholder="CEP" />
-                        <input type="email" id="rua" placeholder="Rua"/>
-                        <input type="email" id="numero" placeholder="Número" />
-                        <input type="email" id="complemento" placeholder="Complemento opcional" />
-                        <input type="email" id="bairro" placeholder="Bairro"/>
-                        <input type="email" id="cidade" placeholder="Cidade"/>
-                        <input type="email" id="estado" placeholder="Estado"/>
-                    </form>
+        <Container>
+            <ContainerCompletePedido>
+                
+                <h1>Complete seu pedido</h1>
+                <ContainerEndereçoEntrega>
+                    <section className="formularioContainer">
+                        <ContentEntrega>
+                             <div className="icon-wrapper">
+                                <MapPinLine size={25} color='#C47F17' />
+                            </div>
+                            <div className="text-container">
+                                <h3>Endereço de Entrega</h3>
+                                <p>Informe o endereço onde deseja receber seu pedido</p>
+                            </div>
+                        </ContentEntrega>
+                        
+                        <ContainerForm>
+                            <Input type="text" id="cep" placeholder="CEP" />
+                            <Input type="text" id="rua" placeholder="Rua"/>
+                            <Input type="text" id="numero" placeholder="Número" />
+                            <Input type="text" id="complemento" placeholder="Complemento opcional" />
+                            <Input type="text" id="bairro" placeholder="Bairro"/>
+                            <Input type="text" id="cidade" placeholder="Cidade"/>
+                            <Input type="text" id="estado" placeholder="Estado"/>
+                        </ContainerForm>
 
 
-                </section>
+                    </section>
 
-                <section>
-                    <h3>
-                        Pagamento
-                    </h3>
-                    <p>O pagamento é feito na entrega.Escolha a forma que deseja pagar</p>
+                    <section>
+                          <ContentPagamento>
+                             <div className="icon-wrapper">
+                                <CurrencyDollar size={25} color='#8047F8' />
+                            </div>
+                            <div className="text-container">
+                                <h3>Pagamento</h3>
+                                <p>O pagamento é feito na entrega.Escolha a forma que deseja pagar</p>
+                            </div>
+                        </ContentPagamento>
+                        
+                        <div>
+                            <button type="button"> gegthr</button>
+                            <button type="button"> gegthr</button>
+                            <button type="button"> gegthr</button>
+                        </div>
+                    </section>
+                </ContainerEndereçoEntrega>  
+            </ContainerCompletePedido>   
+
+
+            <section>
+                <h1>Cafés selecionados</h1>
                     <div>
-                        <button type="button"> gegthr</button>
-                        <button type="button"> gegthr</button>
-                        <button type="button"> gegthr</button>
+                        <div>
+                            <img src="" alt="" />
+                            <div>
+                                <p>wergte</p>
+                                <p>efwg</p>   
+                            </div>
+                            <div>
+                                <button type="button">wrgq</button>
+                                <button type="button">wrgq</button>
+                            </div>
+                        </div>
+
+                        <div>
+                            <div>
+                                <p>rge</p>
+                                <p>sdfg</p>
+                            </div>
+
+                            <div>
+                                <p>rge</p>
+                                <p>sdfg</p>
+                            </div>
+
+                            <div>
+                                <p>rge</p>
+                                <p>sdfg</p>
+                            </div>
+
+                            <footer>
+                                <button type="submit">aefwg</button>
+                            </footer>
+                        </div>
                     </div>
                 </section>
-            </div>  
-        </section>   
-
-
-        <section>
-            <h1>Cafés selecionados</h1>
-                <div>
-                    <div>
-                        <img src="" alt="" />
-                        <div>
-                            <p>wergte</p>
-                            <p>efwg</p>   
-                        </div>
-                        <div>
-                            <button type="button">wrgq</button>
-                            <button type="button">wrgq</button>
-                        </div>
-                    </div>
-
-                    <div>
-                        <div>
-                            <p>rge</p>
-                            <p>sdfg</p>
-                        </div>
-
-                        <div>
-                            <p>rge</p>
-                            <p>sdfg</p>
-                        </div>
-
-                        <div>
-                            <p>rge</p>
-                            <p>sdfg</p>
-                        </div>
-
-                        <footer>
-                            <button type="submit">aefwg</button>
-                        </footer>
-                    </div>
-                </div>
-            </section>
             
-            </>
+            </Container>
     );
 }
--- src/components/Pages/Shopping/styles.ts
+++ src/components/Pages/Shopping/styles.ts
@@ -1,2 +1,148 @@
 import styled from "styled-components";
 
+export const Container= styled.div`
+ display: flex;
+ justify-content: space-between;
+`;
+
+export const ContainerCompletePedido = styled.section `
+flex-direction:column;
+  h1 {
+    color: ${props => props.theme['GRAY-300']};
+  }
+  width: 45rem;
+  height: 36rem;
+`;
+
+export const ContainerEndereçoEntrega =  styled.div`
+margin-top: 1rem;
+  display: flex; /* Para alinhar corretamente os elementos */
+  flex-direction: column;
+  background: ${props => props.theme['WHITE-500']};
+  padding: 3rem;
+  border-radius: 8px;
+  width: 50rem;
+  height: 30rem;
+
+
+  .formularioContainer {
+    display: flex;
+    flex-direction: column;
+    gap: 2.5rem;
+  }
+
+  input {
+    display: flex;
+  }
+`;
+
+export const ContentEntrega = styled.header`
+  display: flex;
+  align-items: center; /* Mantém o ícone e o título alinhados */
+  gap: 0.5rem;
+
+  .icon-wrapper {
+   display: flex;
+    align-items: center; 
+    justify-content: center;
+    
+    /* SubiR o ícone */
+    position: relative;
+    bottom: 13px; 
+  }
+
+  .text-container {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+
+    h3 {
+        font-family: 'Roboto', sans-serif;
+        font-weight: 400;
+        color: ${props => props.theme['GRAY-500']};
+    }
+  }
+`;
+
+
+
+
+export const ContainerForm = styled.form`
+  display: grid;
+  gap: 1rem;
+  grid-template-columns: auto auto auto; /* Ajusta os tamanhos dinamicamente */
+  width: 100%;
+`;
+
+/* Ajuste dos Inputs */
+export const Input = styled.input`
+  padding: 0.8rem;
+  border: 3px solid ${props => props.theme['GRAY-100']};
+  border-radius: 4px;
+  font-size: 0.8rem;
+
+  &.cep {
+    grid-column: span 1; /* Mantém sozinho */
+    width: 16rem;
+  }
+
+  &#rua {
+    grid-column: span 3; /* Ocupa toda a linha */
+   width: 100%;
+  }
+
+  &#numero {
+    width: 16rem;
+  }
+
+  &#complemento {
+    grid-column: span 2;
+    width: 100%;
+  }
+
+  &#bairro {
+    width: 16rem;
+  }
+
+  &#cidade {
+    width: 22rem;
+  }
+
+  &#estado {
+    width: 100%;
+  }
+`;
+
+
+
+
+export const ContentPagamento = styled.div`
+ display: flex;
+  align-items: center; /* Mantém o ícone e o título alinhados */
+  gap: 0.5rem;
+
+  .icon-wrapper {
+   display: flex;
+    align-items: center; 
+    justify-content: center;
+    
+    /* SubiR o ícone */
+    position: relative;
+    bottom: 13px; 
+  }
+
+  .text-container {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+
+    h3 {
+        font-family: 'Roboto', sans-serif;
+        font-weight: 400;
+        color: ${props => props.theme['GRAY-500']};
+    }
+  }
+`;
+
+
+
